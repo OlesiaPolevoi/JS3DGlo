25715e7c87f9e6c2af3f4245cf14ad52affc7388
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-01-11 19:15:54 -0800

    lesson 20 homework done

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
 
+import modal from "./modules/modal";
+
 timer("15 January 2022");
 menu();
+modal();
 // import twoJS from "./modules/two";
 // twoJS();
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -6,23 +6,26 @@ const menu = () => {
   const closeBtn = menu.querySelector(".close-btn");
   const menuItems = menu.querySelectorAll("ul>li>a");
 
-  console.log(menuItems);
+  const handleMenu = () => {
+    // if (!menu.style.transform) {
+    //   menu.style.transform = `translateX(0)`;
+    // } else {
+    //   menu.style.transform = ``;
+    // }
 
-  menuBtn.addEventListener("click", () => {
-    if (!menu.style.transform) {
-      menu.style.transform = `translateX(0)`;
-    } else {
-      menu.style.transform = ``;
-    }
-  });
+    menu.classList.toggle("active-menu");
+  };
+  menuBtn.addEventListener("click", handleMenu);
 
-  closeBtn.addEventListener("click", () => {
-    if (!menu.style.transform) {
-      menu.style.transform = `translateX(0)`;
-    } else {
-      menu.style.transform = ``;
-    }
-  });
+  closeBtn.addEventListener("click", handleMenu);
+
+  // for (let i = 0; i < menuItems.length; i++) {
+  //   menuItems[i].addEventListener("click", handleMenu);
+  // }
+
+  menuItems.forEach((menuItem) =>
+    menuItem.addEventListener("click", handleMenu)
+  );
 };
 
 export default menu;
