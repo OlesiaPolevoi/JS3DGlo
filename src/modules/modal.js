4947a123687f9a27119adf60322a2e827557300b
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-01-15 13:21:14 -0500

    lesson 20 added animation to modal

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -4,9 +4,21 @@ const modal = () => {
   const buttons = document.querySelectorAll(".popup-btn");
   const closeBtn = modal.querySelector(".popup-close");
 
+  const modalContent = document.querySelector(".popup-content");
+
   buttons.forEach((btn) => {
     btn.addEventListener("click", () => {
+      modalContent.style.top = "-52%";
       modal.style.display = "block";
+
+      let count = -52;
+      const modalAnimate = () => {
+        count++;
+
+        modalContent.style.top = count + "%";
+        if (count < 10) setTimeout(modalAnimate, 3);
+      };
+      modalAnimate();
     });
   });
 
