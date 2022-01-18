47f38687b535525ad3d246e91fcad6902bd8090e
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-01-18 12:59:35 -0500

    lesson 21 complete

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -34,6 +34,36 @@ const calculator = () => {
   formThreeName.addEventListener("input", (e) => {
     e.target.value = e.target.value.replace(/[^а-яА-я\-\s]/g, "");
   });
+
+  const formOneEmail = document.getElementById("form1-email");
+  const formTwoEmail = document.getElementById("form2-email");
+  const formThreeEmail = document.getElementById("form3-email");
+
+  formOneEmail.addEventListener("input", (e) => {
+    e.target.value = e.target.value.replace(/[^a-zA-Z\-\_\@\.\!\~\*\']/gi, "");
+  });
+  formTwoEmail.addEventListener("input", (e) => {
+    e.target.value = e.target.value.replace(/[^a-zA-Z\-\_\@\.\!\~\*\']/gi, "");
+  });
+  formThreeEmail.addEventListener("input", (e) => {
+    e.target.value = e.target.value.replace(/[^a-zA-Z\-\_\@\.\!\~\*\']/gi, "");
+  });
+
+  const formOnePhone = document.getElementById("form1-phone");
+  const formTwoPhone = document.getElementById("form2-phone");
+  const formThreePhone = document.getElementById("form3-phone");
+
+  formOnePhone.addEventListener("input", (e) => {
+    e.target.value = e.target.value.replace(/[^\d\-\(\)]/g, "");
+  });
+
+  formTwoPhone.addEventListener("input", (e) => {
+    e.target.value = e.target.value.replace(/[^\d\-\(\)]/g, "");
+  });
+
+  formThreePhone.addEventListener("input", (e) => {
+    e.target.value = e.target.value.replace(/[^\d\-\(\)]/g, "");
+  });
 };
 
 export default calculator;
