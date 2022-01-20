1f14cd55189f490d498c42f378df03e7f8cafeb3
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-01-19 22:55:19 -0500

    started lesson22 classwork

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
 import inputForms from "./modules/inputForms";
+import tabs from "./modules/tabs";
 
 timer("15 January 2022");
 menu();
 modal();
 inputForms();
+tabs();
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,7 +2,6 @@
 const modal = () => {
   const modal = document.querySelector(".popup");
   const buttons = document.querySelectorAll(".popup-btn");
-  const closeBtn = modal.querySelector(".popup-close");
 
   const modalContent = document.querySelector(".popup-content");
 
@@ -34,8 +33,13 @@ const modal = () => {
     });
   });
 
-  closeBtn.addEventListener("click", () => {
-    modal.style.display = "none";
+  modal.addEventListener("click", (e) => {
+    if (
+      !e.target.closest(".popup-content") ||
+      e.target.classList.contains("popup-close")
+    ) {
+      modal.style.display = "none";
+    }
   });
 };
 
