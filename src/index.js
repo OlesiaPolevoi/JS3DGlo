4b9b90adda2f39012cfe5f6ef3b2d7b65e5251ba
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-01-21 09:10:16 -0500

    lesson 22 done

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import menu from "./modules/menu";
 import modal from "./modules/modal";
 import inputForms from "./modules/inputForms";
 import tabs from "./modules/tabs";
+import slider from "./modules/slider";
 
 timer("15 January 2022");
 menu();
 modal();
 inputForms();
 tabs();
+slider();
